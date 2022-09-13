7d0e16ede0cb6625bfc296743c22048fcef8b7f8
Daniel Eyny <danieleyny@gmail.com>
2022-09-13 17:42:24 -0400

    update fixing the partners section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,6 @@
  /*=============== Partners ===============*/
 
- $(document).ready(function(){
-    $('.partners-logos').slick({
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoints: 768,
-            settings: {
-                slidesToShow: 4
-            }
-        }, {
-            breakpoints: 520,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
-    });
-});
+ 
 
 
 
