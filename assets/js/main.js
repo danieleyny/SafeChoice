9d2cac2582d1134cb1e576e3e5f38e5946dfe4de
Daniel Eyny <danieleyny@gmail.com>
2022-10-05 05:34:34 -0400

    edit to image type to help increase website speed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,98 +1,98 @@
- /*=============== Header Color ===============*/
+                /*=============== Header Color ===============*/
 
- 
-// Toggle the .pa-fixed-header class when the user 
-// scroll 100px 
+                
+                // Toggle the .pa-fixed-header class when the user 
+                // scroll 100px 
 
-window.onscroll = () => {scrollNavbar()};
+                window.onscroll = () => {scrollNavbar()};
 
-scrollNavbar = () => {
-    // Target elements
-    const navBar = document.getElementById("header");
+                scrollNavbar = () => {
+                    // Target elements
+                    const navBar = document.getElementById("header");
 
-  if (document.documentElement.scrollTop > 100) {
-    navBar.classList.add("pa-fixed-header");
+                  if (document.documentElement.scrollTop > 100) {
+                    navBar.classList.add("pa-fixed-header");
 
-  
+                  
 
-  } else {
-    navBar.classList.remove("pa-fixed-header");
-    
-   
-  }
-}
- /*=============== Header Color ===============*/
-
-
- 
-/*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
-
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
-}
-
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
-}
+                  } else {
+                    navBar.classList.remove("pa-fixed-header");
+                    
+                  
+                  }
+                }
+                /*=============== Header Color ===============*/
 
 
-/*=============== SERVICES MODAL ===============*/
-const modalViews = document.querySelectorAll('.services__modal'),
-      modalBtns = document.querySelectorAll('.services__button'),
-      modalClose = document.querySelectorAll('.services__modal-close')
+                
+                /*=============== SHOW MENU ===============*/
+                const navMenu = document.getElementById('nav-menu'),
+                      navToggle = document.getElementById('nav-toggle'),
+                      navClose = document.getElementById('nav-close')
 
-let modal = function(modalClick){
-  	modalViews[modalClick].classList.add('active-modal')
-}
+                /*===== MENU SHOW =====*/
+                /* Validate if constant exists */
+                if(navToggle){
+                    navToggle.addEventListener('click', () =>{
+                        navMenu.classList.add('show-menu')
+                    })
+                }
 
-modalBtns.forEach((mb, i) =>{
-	mb.addEventListener('click', () =>{
-		modal(i)
-	})
-})
-
-modalClose.forEach((mc) =>{
-	mc.addEventListener('click', () =>{
-		modalViews.forEach((mv) =>{
-			mv.classList.remove('active-modal')
-		})
-	})
-})
-
+                /*===== MENU HIDDEN =====*/
+                /* Validate if constant exists */
+                if(navClose){
+                    navClose.addEventListener('click', () =>{
+                        navMenu.classList.remove('show-menu')
+                    })
+                }
 
 
-/*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+                /*=============== SERVICES MODAL ===============*/
+                const modalViews = document.querySelectorAll('.services__modal'),
+                      modalBtns = document.querySelectorAll('.services__button'),
+                      modalClose = document.querySelectorAll('.services__modal-close')
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+                let modal = function(modalClick){
+                    modalViews[modalClick].classList.add('active-modal')
+                }
 
-/*=============== GSAP ANIMATION ===============*/
+                modalBtns.forEach((mb, i) =>{
+                  mb.addEventListener('click', () =>{
+                    modal(i)
+                  })
+                })
 
-TweenMax.from('.home__liquid',2, {delay: .7, opacity: 0, y: 200,ease: Expo.easeInOut})
-TweenMax.from('.home__juice-animate', 4, {delay: 1.1, opacity: 0, y: -800,ease: Expo.easeInOut})
-TweenMax.from('.home__apple1', 5.5, {delay: 1.5,opacity: 0,y: -800,ease: Expo.easeInOut})
-TweenMax.from('.home__apple2', 5, {delay: 1.6,opacity: 0,y: -800,ease: Expo.easeInOut})
-TweenMax.from('.home__leaf:nth-child(1)', 6.5, {delay: 1.3,opacity: 0,y: -800,ease: Expo.easeInOut})
-TweenMax.from('.home__leaf:nth-child(2)', 7.5, {delay: 1.4,opacity: 0,y: -800,ease: Expo.easeInOut})
-TweenMax.from('.home__leaf:nth-child(3)', 8.5, {delay: 1.5,opacity: 0,y: -800,ease: Expo.easeInOut})
-TweenMax.from('.home__leaf:nth-child(4)', 8, {delay: 1.6,opacity: 0,y: -800,ease: Expo.easeInOut})
-TweenMax.from('.home__leaf:nth-child(5)', 9.5, {delay: 1.7,opacity: 0,y: -800,ease: Expo.easeInOut})
-TweenMax.from('.home__leaf:nth-child(6)', 9.5, {delay: 1.8,opacity: 0,y: -800,ease: Expo.easeInOut})
+                modalClose.forEach((mc) =>{
+                  mc.addEventListener('click', () =>{
+                    modalViews.forEach((mv) =>{
+                      mv.classList.remove('active-modal')
+                    })
+                  })
+                })
+
+
+
+                /*=============== REMOVE MENU MOBILE ===============*/
+                const navLink = document.querySelectorAll('.nav__link')
+
+                function linkAction(){
+                    const navMenu = document.getElementById('nav-menu')
+                    // When we click on each nav__link, we remove the show-menu class
+                    navMenu.classList.remove('show-menu')
+                }
+                navLink.forEach(n => n.addEventListener('click', linkAction))
+
+                /*=============== GSAP ANIMATION ===============*/
+
+                TweenMax.from('.home__liquid',2, {delay: .7, opacity: 0, y: 200,ease: Expo.easeInOut})
+                TweenMax.from('.home__juice-animate', 4, {delay: 1.1, opacity: 0, y: -800,ease: Expo.easeInOut})
+                TweenMax.from('.home__apple1', 5.5, {delay: 1.5,opacity: 0,y: -800,ease: Expo.easeInOut})
+                TweenMax.from('.home__apple2', 5, {delay: 1.6,opacity: 0,y: -800,ease: Expo.easeInOut})
+                TweenMax.from('.home__leaf:nth-child(1)', 6.5, {delay: 1.3,opacity: 0,y: -800,ease: Expo.easeInOut})
+                TweenMax.from('.home__leaf:nth-child(2)', 7.5, {delay: 1.4,opacity: 0,y: -800,ease: Expo.easeInOut})
+                TweenMax.from('.home__leaf:nth-child(3)', 8.5, {delay: 1.5,opacity: 0,y: -800,ease: Expo.easeInOut})
+                TweenMax.from('.home__leaf:nth-child(4)', 8, {delay: 1.6,opacity: 0,y: -800,ease: Expo.easeInOut})
+                TweenMax.from('.home__leaf:nth-child(5)', 9.5, {delay: 1.7,opacity: 0,y: -800,ease: Expo.easeInOut})
+                TweenMax.from('.home__leaf:nth-child(6)', 9.5, {delay: 1.8,opacity: 0,y: -800,ease: Expo.easeInOut})
 
 
