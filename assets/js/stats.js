7a4c23e7dc0d15373e9f911e8ae3fbefa5fc75df
Daniel Eyny <danieleyny@gmail.com>
2022-11-18 22:08:14 -0500

    update to the websitre products oct

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -524,6 +524,91 @@
   
   
 
+  
+  
+   /*=============== Copy Link - Article 14 ===============*/
+  
+  
+   //the helper function
+   let createCopy14 = function(textToCopy, triggerElementId, callback = null) {
+    //add event listner to elementtrigger
+    let trigger = document.getElementById(triggerElementId);
+    trigger.addEventListener("click", function() {
+      //create the readonly textarea with the text in it and hide it
+      let tarea = document.createElement("textarea");
+      tarea.setAttribute("id", triggerElementId + "-copyarea");
+      tarea.setAttribute("readonly", "readonly");
+      tarea.setAttribute(
+        "style",
+        "opacity: 0; position: absolute; z-index: -1; top: 0; left: -9999px;"
+      );
+      tarea.appendChild(document.createTextNode(textToCopy));
+      document.body.appendChild(tarea);
+  
+      //select and copy the text in the readonly text area
+      tarea.select();
+      document.execCommand("copy");
+  
+      //remove the element from the DOM
+      document.body.removeChild(tarea);
+  
+      //fire callback function if provided
+      if (typeof callback === "function" && callback()) {
+        callback();
+      }
+    });
+  };
+  
+  
+  //usage example
+  createCopy14('https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7154221/', 'elementId14', function () {
+      alert('You have copied the link for this article succesfully!');
+  });
+  
+  
+
+  
+   /*=============== Copy Link - Article 14 ===============*/
+  
+  
+   //the helper function
+   let createCopy15 = function(textToCopy, triggerElementId, callback = null) {
+    //add event listner to elementtrigger
+    let trigger = document.getElementById(triggerElementId);
+    trigger.addEventListener("click", function() {
+      //create the readonly textarea with the text in it and hide it
+      let tarea = document.createElement("textarea");
+      tarea.setAttribute("id", triggerElementId + "-copyarea");
+      tarea.setAttribute("readonly", "readonly");
+      tarea.setAttribute(
+        "style",
+        "opacity: 0; position: absolute; z-index: -1; top: 0; left: -9999px;"
+      );
+      tarea.appendChild(document.createTextNode(textToCopy));
+      document.body.appendChild(tarea);
+  
+      //select and copy the text in the readonly text area
+      tarea.select();
+      document.execCommand("copy");
+  
+      //remove the element from the DOM
+      document.body.removeChild(tarea);
+  
+      //fire callback function if provided
+      if (typeof callback === "function" && callback()) {
+        callback();
+      }
+    });
+  };
+  
+  
+  //usage example
+  createCopy15('https://www.ncbi.nlm.nih.gov/books/NBK279323/', 'elementId15', function () {
+      alert('You have copied the link for this article succesfully!');
+  });
+  
+  
+
 
 
    /*=============== Copy Link - End ===============*/
