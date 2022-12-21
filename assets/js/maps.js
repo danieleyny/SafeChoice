3c19e939a660895c5124b2758130e773129ed9c4
Daniel Eyny <danieleyny@gmail.com>
2022-12-21 02:41:21 -0500

    zip code search test

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -43,6 +43,20 @@ var marker1 = new google.maps.Marker({
   });
 
   
+  // Add another marker to the map
+  var marker6 = new google.maps.Marker({
+    position: {lat:  40.82631196024513, lng: -73.94738180476277},
+    map: map,
+  });
+
+
+  // Add another marker to the map
+  var marker7 = new google.maps.Marker({
+    position: {lat:  40.78030963353353, lng: -73.94658410053034},
+    map: map,
+  });
+
+  
   
   
 
@@ -104,6 +118,34 @@ var marker1 = new google.maps.Marker({
 
   
 
+  
+  // Add a click event listener to the second marker
+  marker6.addListener('click', function() {
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      // Get directions from the user's current location to the second address
+      getDirections(new google.maps.LatLng(lat, lng), '1734 Amsterdam Ave, New York, NY 10031');
+    });
+  });
+
+  
+
+  
+  // Add a click event listener to the second marker
+  marker7.addListener('click', function() {
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      // Get directions from the user's current location to the second address
+      getDirections(new google.maps.LatLng(lat, lng), '1758 1st Ave., New York, NY 10128');
+    });
+  });
+
+  
+
   // Set the map for the directions renderer
   directionsRenderer.setMap(map);
 }
@@ -129,3 +171,61 @@ function getDirections(origin, destination) {
 }
 
 window.addEventListener('load', initMap);
+
+
+
+// Add an event listener to the form's submit event
+document.getElementById('zip-code-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Get the user's zip code from the form
+  var zipCode = document.getElementById('zip-code').value;
+
+  // Send an HTTP request to a server-side script or API endpoint with the user's zip code as a parameter
+  fetch('/api/markers?zipCode=' + zipCode)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(markers) {
+      // Display the list of markers to the user
+      displayMarkers(markers);
+    });
+});
+
+
+
+function displayMarkers(markers) {
+  // Clear any existing markers from the map
+  clearMarkers();
+
+  // Add the markers to the map
+  markers.forEach(function(marker) {
+    addMarker(marker);
+  });
+}
+
+function addMarker(marker) {
+  // Create a new marker on the map
+  var mapMarker = new google.maps.Marker({
+    position: {lat: marker.lat, lng: marker.lng},
+    map: map
+  });
+
+  // Add the marker to the list of markers
+  markers.push(mapMarker);
+}
+
+function clearMarkers() {
+  // Remove the markers from the map
+  markers.forEach(function(marker) {
+    marker.setMap(null);
+  });
+
+  // Clear the list of markers
+  markers = [];
+}
+
+
+
+
+
