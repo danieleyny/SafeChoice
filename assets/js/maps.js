a298d427e64603e0786087fe45e77fe7447261f8
Daniel Eyny <danieleyny@gmail.com>
2022-12-21 21:17:26 -0500

    additional map for colleges

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -75,7 +75,7 @@ function initMap() {
       var lat = position.coords.latitude;
       var lng = position.coords.longitude;
       // Get directions from the user's current location to the first address
-      getDirections(new google.maps.LatLng(lat, lng), '215 West 101st, New York, NY 10025');
+      getDirections(new google.maps.LatLng(lat, lng), '214 West 102st, New York, NY 10025');
     });
   });
 
@@ -86,7 +86,7 @@ function initMap() {
       var lat = position.coords.latitude;
       var lng = position.coords.longitude;
       // Get directions from the user's current location to the second address
-      getDirections(new google.maps.LatLng(lat, lng), '214 West 102nd, New York, NY 10025');
+      getDirections(new google.maps.LatLng(lat, lng), '215 West 101st, New York, NY 10025');
     });
   });
 
@@ -179,9 +179,409 @@ function getDirections(origin, destination) {
 }
 
 
-
-
 window.addEventListener('load', initMap);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function initMapTest() {
+  // Create the map
+  var map = new google.maps.Map(document.getElementById('mapTest'), {
+    zoom: 11.5,
+    center: {lat: 40.7977815, lng: -73.9711954}
+  });
+
+
+
+  // Add a marker to the map
+  var markerColumbia = new google.maps.Marker({
+    position: {lat: 40.80409446608371, lng: -73.96858177791212},
+    map: map,
+    icon: new google.maps.MarkerImage(
+      'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + '9BCBEB',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0,0),
+      new google.maps.Point(10, 34)
+    )
+  });
+  
+
+
+
+  // Add a marker to the map
+  var markerColumbia2 = new google.maps.Marker({
+    position: {lat: 40.80382310972846, lng: -73.96589774655698},
+    map: map,
+    icon: new google.maps.MarkerImage(
+      'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + '9BCBEB',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0,0),
+      new google.maps.Point(10, 34)
+    )
+  });
+ 
+  
+
+  // Add a marker to the map
+  var markerColumbia4 = new google.maps.Marker({
+    position: {lat: 40.80673127558823, lng: -73.964032573546},
+    map: map,
+    icon: new google.maps.MarkerImage(
+      'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + '9BCBEB',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0,0),
+      new google.maps.Point(10, 34)
+    )
+  });
+  
+  
+  
+  
+  // Add a marker to the map
+  var markerColumbia5 = new google.maps.Marker({
+    position: {lat: 40.80565847308409, lng: -73.96344367354602},
+    map: map,
+    icon: new google.maps.MarkerImage(
+      'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + '9BCBEB',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0,0),
+      new google.maps.Point(10, 34)
+    )
+  });
+  
+  
+  
+
+  // Add a marker to the map
+  var markerColumbia3 = new google.maps.Marker({
+    position: {lat: 40.80827675472973, lng: -73.96544474471074},
+    map: map,
+    icon: new google.maps.MarkerImage(
+      'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + '9BCBEB',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0,0),
+      new google.maps.Point(10, 34)
+    )
+  });
+  
+
+
+  // Add a marker to the map
+  var markerHunter = new google.maps.Marker({
+    position: {lat:  40.78366509381712, lng: -73.95230550046314},
+    map: map,
+    icon: new google.maps.MarkerImage(
+      'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + '3F0157',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0,0),
+      new google.maps.Point(10, 34)
+    )
+  });
+  
+
+   
+  // Add a marker to the map
+  var markerHunter2 = new google.maps.Marker({
+    position: {lat:  40.78639168641162, lng: -73.94898903121697},
+    map: map,
+    icon: new google.maps.MarkerImage(
+      'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + '3F0157',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0,0),
+      new google.maps.Point(10, 34)
+    )
+  });
+  
+  
+
+  // Add a marker to the map
+  var markerHunter3 = new google.maps.Marker({
+    position: {lat:  40.78603895989781, lng: -73.9541184177224},
+    map: map,
+    icon: new google.maps.MarkerImage(
+      'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + '3F0157',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0,0),
+      new google.maps.Point(10, 34)
+    )
+  });
+  
+
+  
+  // Add another marker to the map
+  var marker2 = new google.maps.Marker({
+    position: {lat: 40.79796016191922, lng: -73.96897451565641},
+    map: map,
+  });
+
+  
+  // Add another marker to the map
+  var marker3 = new google.maps.Marker({
+    position: {lat: 40.79953273485606, lng: -73.96478975581769},
+    map: map,
+  });
+
+  
+  // Add another marker to the map
+  var marker4 = new google.maps.Marker({
+    position: {lat: 40.79934945635314, lng: -73.96483872148701},
+    map: map,
+  });
+
+  
+  // Add another marker to the map
+  var marker5 = new google.maps.Marker({
+    position: {lat: 40.83372005109081, lng: -73.94480573992801},
+    map: map,
+  });
+
+  
+  // Add another marker to the map
+  var marker6 = new google.maps.Marker({
+    position: {lat:  40.82631196024513, lng: -73.94738180476277},
+    map: map,
+  });
+
+
+  // Add another marker to the map
+  var marker7 = new google.maps.Marker({
+    position: {lat:  40.78030963353353, lng: -73.94658410053034},
+    map: map,
+  });
+
+  
+  
+  
+
+  // Add a click event listener to the first marker
+  markerColumbia.addListener('click', function() {
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      // Get directions from the user's current location to the first address
+      getDirections(new google.maps.LatLng(lat, lng), '362 Riverside Drive, New York, NY 10027');
+    });
+  });
+
+  
+  // Add a click event listener to the first marker
+  markerColumbia2.addListener('click', function() {
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      // Get directions from the user's current location to the first address
+      getDirections(new google.maps.LatLng(lat, lng), '544 W. 110th Street, New York, NY 10027');
+    });
+  });
+  
+  // Add a click event listener to the first marker
+  markerColumbia3.addListener('click', function() {
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      // Get directions from the user's current location to the first address
+      getDirections(new google.maps.LatLng(lat, lng), '627 West 115th Street, New York, NY 10027');
+    });
+  });
+  
+  // Add a click event listener to the first marker
+  markerColumbia4.addListener('click', function() {
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      // Get directions from the user's current location to the first address
+      getDirections(new google.maps.LatLng(lat, lng), '545 West 114th Street, New York, NY 10027');
+    });
+  });
+  
+  // Add a click event listener to the first marker
+  markerColumbia5.addListener('click', function() {
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      // Get directions from the user's current location to the first address
+      getDirections(new google.maps.LatLng(lat, lng), '523 W 113th St., New York, NY 10027');
+    });
+  });
+
+
+  
+  // Add a click event listener to the first marker
+  markerHunter.addListener('click', function() {
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      // Get directions from the user's current location to the first address
+      getDirections(new google.maps.LatLng(lat, lng), '1395 Lexington Avenue, New York, NY 10128');
+    });
+  });
+
+
+  
+  // Add a click event listener to the first marker
+  markerHunter2.addListener('click', function() {
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      // Get directions from the user's current location to the first address
+      getDirections(new google.maps.LatLng(lat, lng), '1760 3rd Avenue, New York, NY 10029');
+    });
+  });
+
+
+  
+  // Add a click event listener to the first marker
+  markerHunter3.addListener('click', function() {
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      // Get directions from the user's current location to the first address
+      getDirections(new google.maps.LatLng(lat, lng), '71 East 94th Street, New York, NY 10128');
+    });
+  });
+
+
+
+  // Add a click event listener to the second marker
+  marker2.addListener('click', function() {
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      // Get directions from the user's current location to the second address
+      getDirections(new google.maps.LatLng(lat, lng), '215 West 101st, New York, NY 10025');
+    });
+  });
+
+  // Add a click event listener to the second marker
+  marker3.addListener('click', function() {
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      // Get directions from the user's current location to the second address
+      getDirections(new google.maps.LatLng(lat, lng), '147 W 105th St, New York, NY 10025');
+    });
+  });
+
+  // Add a click event listener to the second marker
+  marker4.addListener('click', function() {
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      // Get directions from the user's current location to the second address
+      getDirections(new google.maps.LatLng(lat, lng), '145 W 105th St, New York, NY 10025');
+    });
+  });
+
+  
+  // Add a click event listener to the second marker
+  marker5.addListener('click', function() {
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      // Get directions from the user's current location to the second address
+      getDirections(new google.maps.LatLng(lat, lng), '3770 Broadway, New York, NY 10032');
+    });
+  });
+
+  
+
+  
+  // Add a click event listener to the second marker
+  marker6.addListener('click', function() {
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      // Get directions from the user's current location to the second address
+      getDirections(new google.maps.LatLng(lat, lng), '1734 Amsterdam Ave, New York, NY 10031');
+    });
+  });
+
+  
+
+  
+  // Add a click event listener to the second marker
+  marker7.addListener('click', function() {
+    // Get the user's current location
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      // Get directions from the user's current location to the second address
+      getDirections(new google.maps.LatLng(lat, lng), '1758 1st Ave., New York, NY 10128');
+    });
+  });
+
+  
+
+  // Set the map for the directions renderer
+  directionsRenderer.setMap(map);
+}
+
+
+
+// Function to get directions to an address
+function getDirections(origin, destination) {
+  // Use the Directions API to get directions from the origin to the destination
+  var request = {
+    origin: origin,
+    destination: destination,
+    travelMode: 'DRIVING'
+  };
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      // Display the directions on the map
+      directionsRenderer.setDirections(result);
+    } else {
+      // Handle error
+    }
+  });
+}
+
+
+window.addEventListener('load', initMapTest);
 
 
 
