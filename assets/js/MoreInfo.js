1b0e1b5a1cb4bc12c2fec3c4e9ba69c08b19a0de
Daniel Eyny <danieleyny@gmail.com>
2023-01-22 23:39:25 -0600

    seperation of moreinfo section to its own file for speed

---
+++ assets/js/MoreInfo.js
@@ -0,0 +1,324 @@
+
+// More Info Button
+
+
+function myMoreInfoBtn() {
+  
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  
+  function myMoreInfoBtn2() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("myBtn2");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  
+  function myMoreInfoBtn3() {
+    var dots = document.getElementById("dots3");
+    var moreText = document.getElementById("more3");
+    var btnText = document.getElementById("myBtn3");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  
+  function myMoreInfoBtn4() {
+    var dots = document.getElementById("dots4");
+    var moreText = document.getElementById("more4");
+    var btnText = document.getElementById("myBtn4");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  
+  
+  function myMoreInfoBtn5() {
+    var dots = document.getElementById("dots5");
+    var moreText = document.getElementById("more5");
+    var btnText = document.getElementById("myBtn5");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  
+  
+  function myMoreInfoBtn6() {
+    var dots = document.getElementById("dots6");
+    var moreText = document.getElementById("more6");
+    var btnText = document.getElementById("myBtn6");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  
+  function myMoreInfoBtn7() {
+    var dots = document.getElementById("dots7");
+    var moreText = document.getElementById("more7");
+    var btnText = document.getElementById("myBtn7");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  
+  
+  
+  function myMoreInfoBtn8() {
+    var dots = document.getElementById("dots8");
+    var moreText = document.getElementById("more8");
+    var btnText = document.getElementById("myBtn8");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  
+  
+  function myMoreInfoBtn9() {
+    var dots = document.getElementById("dots9");
+    var moreText = document.getElementById("more9");
+    var btnText = document.getElementById("myBtn9");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  
+  
+  
+  function myMoreInfoBtn10() {
+    var dots = document.getElementById("dots10");
+    var moreText = document.getElementById("more10");
+    var btnText = document.getElementById("myBtn10");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  
+  
+  
+  function myMoreInfoBtn11() {
+    var dots = document.getElementById("dots11");
+    var moreText = document.getElementById("more11");
+    var btnText = document.getElementById("myBtn11");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  
+  
+  function myMoreInfoBtn12() {
+    var dots = document.getElementById("dots12");
+    var moreText = document.getElementById("more12");
+    var btnText = document.getElementById("myBtn12");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  
+  function myMoreInfoBtn13() {
+    var dots = document.getElementById("dots13");
+    var moreText = document.getElementById("more13");
+    var btnText = document.getElementById("myBtn13");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  
+  
+  
+  function myMoreInfoBtn14() {
+    var dots = document.getElementById("dots14");
+    var moreText = document.getElementById("more14");
+    var btnText = document.getElementById("myBtn14");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  
+  
+  function myMoreInfoBtn15() {
+    var dots = document.getElementById("dots15");
+    var moreText = document.getElementById("more15");
+    var btnText = document.getElementById("myBtn15");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  
+  function myMoreInfoBtn16() {
+    var dots = document.getElementById("dots16");
+    var moreText = document.getElementById("more16");
+    var btnText = document.getElementById("myBtn16");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  function myMoreInfoBtn17() {
+    var dots = document.getElementById("dots17");
+    var moreText = document.getElementById("more17");
+    var btnText = document.getElementById("myBtn17");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
+  
+  
+  
+  
+  function myMoreInfoBtn18() {
+    var dots = document.getElementById("dots18");
+    var moreText = document.getElementById("more18");
+    var btnText = document.getElementById("myBtn18");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<i class='bx bx-chevron-right services__modal-icon' ></i>"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<i class='bx bx-chevron-down services__modal-icon' ></i>"; 
+      moreText.style.display = "inline";
+    }
+  }  
